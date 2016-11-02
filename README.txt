c9e6c5847d74180c90dede6f13138d2cdc0212a9
Melissa Kepler <mmkepler@yahoo.com>
2016-11-02 11:09:23 -0400

    weather app 1

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/mmkepler/pen/jqwqpJ.
+
+ This is my Free Code Camp weather app project. I used used the Wunderground API.
---
+++ js/index.js
@@ -0,0 +1,95 @@
+//Initial code to retrieve Location and Weather in English/Imperial
+$(document).ready(function() {
+  var lati, longi;
+
+  //gather geolocation data from client
+  navigator.geolocation.getCurrentPosition(successFunc, errorFunc, {
+    maximumAge: 600000
+  });
+
+  function successFunc(position) {
+    longi = position.coords.longitude;
+    lati = position.coords.latitude;
+    //my Open Weather Map API id
+    var myId = "4a82154c39d11213";
+    var wunderground = 'https://api.wunderground.com/api/' + myId + '/conditions/geolocation/q/' + lati + ',' + longi + '.json';
+    // request for Open Weather Map
+    $.ajax(wunderground)
+      .done(
+        function(data) {
+         //put information from object sent into variables
+          var temperature = Math.floor(data.current_observation.temp_f);
+          var cityName = data.current_observation.display_location.city;
+          var country = data.current_observation.display_location.country;
+          var weatherDescription = data.current_observation.weather;
+          var wind = data.current_observation.wind_mph;
+          var humidity = data.current_observation.relative_humidity;
+          var iconLink = "https://crossorigin.me/" + data.current_observation.icon_url;
+          var iconImage = ('<img>');
+          iconImage = $(iconImage).attr("src", iconLink).css("height", "53px");
+          var wunderImgLink = "https://crossorigin.me/" + data.current_observation.image.url;
+          $("#wunderImg").attr("src", wunderImgLink).css("width", "144px");
+          //code for display of current time
+          var today = new Date();
+          var currentTime = new Date();
+          var hours = currentTime.getHours();
+          var minutes = currentTime.getMinutes();
+          var ending = "AM";
+
+          if (minutes < 10) {
+            minutes = "0" + minutes;
+          }
+          if (hours >= 12) {
+            hours = hours - 12;
+            ending = "PM";
+          }
+
+          if (hours === 0) {
+            hours = 12;
+          }
+          var currentDate = new Date();
+          var timeNow = hours + ":" + minutes + " " + ending;
+
+          //insert weather and time data into divs
+          $("#time").html(timeNow + " " + '<br>' + currentDate.toDateString());
+          $("#city").html(cityName + ", " + country);
+          $("#temperature").html(temperature);
+          $("#icon").append(iconImage);
+          $("#description").html(weatherDescription);
+          $("#wind").html(wind);
+          $("#humidity").html(humidity);
+          $("#tempUnit").html("F");
+          $("#windUnit").html("mph");
+          // function for celcius conversion
+          $("#celsius").click(function(event) {
+            event.preventDefault();
+            $("#fahrenheit").css("border", "none");
+            $("#temperature").html(Math.floor((((temperature - 32) * 5) / 9)));
+            $("#wind").html(Math.round(wind * 1.60934));
+            $("#celsius").css("border", "3px solid black");
+            $("#tempUnit").html("C");
+            $("#windUnit").html("kph");
+          });
+          //function for fahrenheit
+          $("#fahrenheit").click(function(event) {
+            event.preventDefault();
+            $("#celsius").css("border", "none");
+            temperature = temperature;
+            $("#temperature").html(temperature);
+            $("#wind").html(wind);
+            $("#fahrenheit").css("border", "3px solid black");
+            $("#tempUnit").html("F");
+            $("#windUnit").html("mph");
+
+          });
+
+        }).fail(
+        function() {
+          $('#communication').html('Error, please try again.');
+        });
+  }
+
+  function errorFunc(error) {
+    $("#communication").html("Please allow us to access<br>your location data");
+  }
+});
